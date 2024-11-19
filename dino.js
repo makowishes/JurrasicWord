5c2e67441b7f6bfda62753ef616a57c77b246d6b
rosanajosepha <161498244+rosanajosepha@users.noreply.github.com>
2024-11-19 16:59:59 -0500

    Update extreme music

--- dino.js
+++ dino.js
@@ -215,7 +215,7 @@ function startGame() {
     } else if (currentDifficulty === 'hard') {
         backgroundMusic = new Audio('./assets/music/theme3.mp3');
     }else if (currentDifficulty === 'extreme') {
-        backgroundMusic = new Audio('./assets/music/theme4-200bpm.mp3');
+        backgroundMusic = new Audio('./assets/music/theme4.mp3');
     }
 
     // Configure and play the music
